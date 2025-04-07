f74818af56e5752ed7ca85767806bd3658cc24b8
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-04-07 14:38:48 -0700

    with new data source as kurt_sharing

--- data/curation_scripts/compile_curated_data.py
+++ data/curation_scripts/compile_curated_data.py
@@ -16,13 +16,11 @@ def check_duplicates(df, subset):
 
 
 # Load the datasets
-cbr = pd.read_csv("data/curation_scripts/cbr/cbr_curated.csv")
-pop = pd.read_csv("data/curation_scripts/pop/dpt_district_summaries_curated.csv")
-pop = pop[["adm0_name", "adm1_name", "adm2_name", "dot_name", "year", "under5_pop"]]
+demog = pd.read_csv("data/curation_scripts/pop/demog_data_POLIS_ADM02_yearly_cbr.csv")  # cbr & pop
 ri = pd.read_csv("data/curation_scripts/ri/ri_curated.csv")
 re = pd.read_csv("data/curation_scripts/random_effects/random_effects_curated.csv")
-# sia = pd.read_csv("data/curation_scripts/sia/sia_random_effects_curated.csv")
 underwt = pd.read_csv("data/curation_scripts/individual_risk/underwt_u5_2019.csv")
+
 # Load the shapefiles for validation purposes
 shp0 = gpd.read_file("data/shp_africa_low_res.gpkg", layer="adm0")  # gpd.read_file("data/shp_africa_low_res.gpkg", layer="adm0")
 shp1 = gpd.read_file("data/shp_africa_low_res.gpkg", layer="adm1")  # gpd.read_file("data/shp_africa_adm1.geojson")
@@ -32,14 +30,14 @@ n_adm1 = len(shp1)
 n_adm2 = len(shp2)
 
 # print(cbr.head())
-# print(pop.head())
+# print(demog.head())
 # print(ri.head())
 # print(sia.head())
 # print(underwt.head())
 
 # # Print lengths of datasets
 # print(f'Length of cbr: {len(cbr)}')
-# print(f'Length of pop: {len(pop)}')
+# print(f'Length of demog: {len(demog)}')
 # print(f'Length of ri: {len(ri)}')
 # print(f'Length of sia: {len(sia)}')
 # print(f'Length of underwt: {len(underwt)}')
@@ -51,34 +49,24 @@ assert len(shp2["dot_name"].unique()) == len(shp2["guid"].unique()), (
     "The number of unique dot_name and guid are not equal in the shp2 dataset."
 )
 
-### Validate the cbr dataset
-# print(cbr.head())
-# cbr should have values for each country and year
-n_years_cbr = len(cbr["year"].unique())
-assert len(cbr) == n_adm0 * n_years_cbr, "The cbr dataset does not have values for each country and year."
-n_na_cbr = cbr.isna().sum()
-assert n_na_cbr.sum() == 0, "There are missing values in the cbr dataset. Please check the data sources and curate the missing values."
-# Check for duplicates
-check_duplicates(cbr, ["adm0_name", "year"])
 
-
-### Validate the pop dataset
-# print(pop.head())
-# Ensure that there are no missing values in the pop dataset
-n_na_pop = pop.isna().sum()
-assert n_na_pop.sum() == 0, "There are missing values in the pop dataset. Please check the data sources and curate the missing values."
-# Ensure that every dot_name in the shp2 dataset is in the pop dataset
-assert shp2["dot_name"].isin(pop["dot_name"]).all(), "Not every dot_name in the shp2 dataset is in the pop dataset."
-# Warn if the pop dataset has extra dot_names
-extra_dot_names = pop[~pop["dot_name"].isin(shp2["dot_name"])]["dot_name"].unique()
+### Validate the demog dataset with cbr & demog
+# print(demog.head())
+# Ensure that there are no missing values in the demog dataset
+n_na_demog = demog.isna().sum()
+assert n_na_demog.sum() == 0, "There are missing values in the demog dataset. Please check the data sources and curate the missing values."
+# Ensure that every dot_name in the shp2 dataset is in the demog dataset
+assert shp2["dot_name"].isin(demog["dot_name"]).all(), "Not every dot_name in the shp2 dataset is in the demog dataset."
+# Warn if the demog dataset has extra dot_names
+extra_dot_names = demog[~demog["dot_name"].isin(shp2["dot_name"])]["dot_name"].unique()
 if len(extra_dot_names) > 0:
     print(
-        f"Warning: There are {len(extra_dot_names)} extra dot_names in the pop dataset that are not in the shp2 dataset. These extra dot_names will be dropped."
+        f"Warning: There are {len(extra_dot_names)} extra dot_names in the demog dataset that are not in the shp2 dataset. These extra dot_names will be dropped."
     )
     # Drop any dot_names that aren't in the shp2 dataset
-    pop = pop[pop["dot_name"].isin(shp2["dot_name"])]
+    demog = demog[demog["dot_name"].isin(shp2["dot_name"])]
 # Check for duplicates
-check_duplicates(pop, ["adm0_name", "adm1_name", "adm2_name", "dot_name", "year"])
+check_duplicates(demog, ["adm0_name", "adm1_name", "adm2_name", "dot_name", "year"])
 
 
 ### Validate the ri dataset
@@ -108,41 +96,32 @@ assert len(re) == n_adm1, "The re dataset does not have values for each ADM1."
 check_duplicates(re, ["adm0_name", "adm1_name"])
 
 
-# ### Validate the sia dataset
-# print(sia.head())
-# # sia should have values for each ADM1
-# assert len(sia) == n_adm1, "The sia dataset does not have values for each ADM1."
-# # Check for duplicates
-# check_duplicates(sia, ["adm0_name", "adm1_name"])
-
-
-# ### Validate the underwt dataset
-# # print(underwt.head())
-# # Ensure that there are no missing values in the underwt dataset
-# n_na_underwt = underwt["prop_underwt"].isna().sum()
-# assert n_na_underwt.sum() == 0, (
-#     "There are missing values in the underwt dataset. Please check the data sources and curate the missing values."
-# )
-# # Ensure that every dot_name in the shp2 dataset is in the underwt dataset
+### Validate the underwt dataset
+# print(underwt.head())
+# Ensure that there are no missing values in the underwt dataset
+n_na_underwt = underwt["prop_underwt"].isna().sum()
+assert n_na_underwt.sum() == 0, (
+    "There are missing values in the underwt dataset. Please check the data sources and curate the missing values."
+)
+# Ensure that every dot_name in the shp2 dataset is in the underwt dataset
 # assert shp2["dot_name"].isin(underwt["dot_name"]).all(), "Not every dot_name in the shp2 dataset is in the underwt dataset."
-# # Warn if the underwt dataset has extra dot_names
-# extra_dot_names = underwt[~underwt["dot_name"].isin(shp2["dot_name"])]["dot_name"].unique()
-# if len(extra_dot_names) > 0:
-#     print(
-#         f"Warning: There are {len(extra_dot_names)} extra dot_names in the underwt dataset that are not in the shp2 dataset. These extra dot_names will be dropped."
-#     )
-#     # Drop any dot_names that aren't in the shp2 dataset
-#     underwt = underwt[underwt["dot_name"].isin(shp2["dot_name"])]
-# # Check for duplicates
-# check_duplicates(underwt, ["dot_name"])
+# Warn if the underwt dataset has extra dot_names
+extra_dot_names = underwt[~underwt["dot_name"].isin(shp2["dot_name"])]["dot_name"].unique()
+if len(extra_dot_names) > 0:
+    print(
+        f"Warning: There are {len(extra_dot_names)} extra dot_names in the underwt dataset that are not in the shp2 dataset. These extra dot_names will be dropped."
+    )
+    # Drop any dot_names that aren't in the shp2 dataset
+    underwt = underwt[underwt["dot_name"].isin(shp2["dot_name"])]
+# Check for duplicates
+check_duplicates(underwt, ["dot_name"])
 
 
 ### Merge the datasets
-df = cbr.merge(pop, on=["adm0_name", "year"])
-df = df.merge(ri, on=["adm0_name", "adm1_name", "adm2_name", "dot_name", "year"])
+df = demog.copy()
+df = df.merge(ri, on=["adm0_name", "adm1_name", "adm2_name", "dot_name", "year"], how="left")
 df = df.merge(re, on=["adm0_name", "adm1_name"], how="left")
-# df = df.merge(sia, on=["adm0_name", "adm1_name"], how="left")
-# df = df.merge(underwt[["dot_name", "prop_underwt"]], on="dot_name", how="left")
+df = df.merge(underwt[["dot_name", "prop_underwt"]], on="dot_name", how="left")
 # Reorder
 df = df[
     [
@@ -152,14 +131,22 @@ df = df[
         "dot_name",
         "year",
         "cbr",
-        "under5_pop",
+        "pop_total",
         "immunity_ri_nOPV2",
         "reff_random_effect",
         "sia_random_effect",
+        "prop_underwt",
     ]
 ]
 print(df.head())
 print(f"Length of df: {len(df)}")
+
+
+# Fill in missing values with the ri data cuz it's only for the latest year
+df.loc[:, "immunity_ri_nOPV2"] = df.groupby(["adm0_name", "adm1_name", "adm2_name"])["immunity_ri_nOPV2"].transform(
+    lambda x: x.fillna(x.mean())
+)
+df.loc[:, "prop_underwt"] = df.groupby(["adm0_name", "adm1_name"])["immunity_ri_nOPV2"].transform(lambda x: x.fillna(x.mean()))
 
 
 ### Validate the merged dataset
@@ -170,12 +157,13 @@ assert n_na_df.sum() == 0, "There are missing values in the merged dataset. Plea
 assert shp2["dot_name"].isin(df["dot_name"]).all(), "Not every dot_name in the shp2 dataset is in the merged dataset."
 # Ensure that the merged dataset is the expected length
 n_years_df = len(df["year"].unique())
-assert len(df) == n_adm2 * n_years_cbr, "The merged dataset does not have values for each dot_name and year."
+n_years_demog = len(demog["year"].unique())
+assert len(df) == n_adm2 * n_years_demog, "The merged dataset does not have values for each dot_name and year."
 
 
 ### Tidy up
 df = df.copy()  # Create a copy of the DataFrame
-df.rename(columns={"under5_pop": "pop_u5", "immunity_ri_nOPV2": "ri_eff"}, inplace=True)
+df.rename(columns={"immunity_ri_nOPV2": "ri_eff"}, inplace=True)
 df.head()
 
 
