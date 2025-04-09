0a6b159bd4c147f495c4c516c82f403f6d9ab907
Jonathan Bloedow <jonathan.bloedow@gatesfoundation.org>
2025-04-09 12:28:10 -0700

    Adapted Dockerfile. Also switch to laser.py instead of setup_sim_v2.py in calibrate.py. May revert that.

--- calib/calibrate.py
+++ calib/calibrate.py
@@ -1,32 +1,31 @@
 import click
 from logic import run_worker_main
-
-import laser_polio as lp
+from pathlib import Path
 
 # TODO: Pass the process_data function to the calibrator
 
 
 # ------------------- USER CONFIG -------------------
 num_trials = 2
-study_name = "calib_zamfara_r0"
-calib_config_path = lp.root / "calib/calib_configs/calib_pars_r0.yaml"
-model_config_path = lp.root / "calib/model_configs/config_zamfara.yaml"
-sim_path = lp.root / "calib/setup_sim.py"
-results_path = lp.root / "calib/results" / study_name
+study_name = "my_polio_calibration_study"
+calib_config_path = "calib_pars.yaml"
+model_config_path = "config.yaml"
+sim_path = "laser.py"
+results_path = Path( "calib/results" ) / study_name
 params_file = "params.json"
-actual_data_file = lp.root / "examples/calib_demo_zamfara/synthetic_infection_counts_zamfara_r14.csv"
+actual_data_file = "examples/calib_demo/synthetic_infection_counts.csv"
 # ---------------------------------------------------
 
 
 @click.command()
-@click.option("--study-name", default=study_name, help="Name of the Optuna study.")
-@click.option("--num-trials", default=num_trials, type=int, help="Number of optimization trials.")
-@click.option("--calib-config", default=str(calib_config_path), help="Path to calibration parameter file.")
-@click.option("--model-config", default=str(model_config_path), help="Path to model base config.")
-@click.option("--results-path", default=str(results_path))
-@click.option("--sim-path", default=str(sim_path))
-@click.option("--params-file", default=str(params_file))
-@click.option("--actual-data-file", default=str(actual_data_file))
+@click.option("--study-name", default=study_name, show_default=True, help="Name of the Optuna study.")
+@click.option("--num-trials", default=num_trials, show_default=True, type=int, help="Number of optimization trials.")
+@click.option("--calib-config", default=str(calib_config_path), show_default=True, help="Path to calibration parameter file.")
+@click.option("--model-config", default=str(model_config_path), show_default=True, help="Path to model base config.")
+@click.option("--results-path", default=str(results_path), show_default=True )
+@click.option("--sim-path", default=str(sim_path), show_default=True )
+@click.option("--params-file", default=str(params_file), show_default=True )
+@click.option("--actual-data-file", default=str(actual_data_file), show_default=True )
 def run_worker(**kwargs):
     run_worker_main(**kwargs)
 
