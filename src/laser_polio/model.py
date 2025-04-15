280267906edab986671f279d3c3c9a941b06e1cc
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-04-15 08:54:03 -0700

    correcting seed logic

--- src/laser_polio/model.py
+++ src/laser_polio/model.py
@@ -50,7 +50,7 @@ class SEIR_ABM:
         if self.verbose >= 1:
             sc.printcyan("Initializing simulation...")
 
-        if pars.seed is not None:
+        if pars.seed is None:
             now = datetime.now()  # noqa: DTZ005
             pars.seed = now.microsecond ^ int(now.timestamp())
             sc.printred(f"No seed provided. Using random seed of {pars.seed}.")
