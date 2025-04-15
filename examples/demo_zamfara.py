c3e6bb7aa68bce98bd608f551bfae3d37b6a1523
Steve Kroiss <55462700+skroiss-idm@users.noreply.github.com>
2025-04-15 08:23:58 -0700

    cleaning up demo_nigeria

--- examples/demo_zamfara.py
+++ examples/demo_zamfara.py
@@ -2,19 +2,6 @@ import sciris as sc
 
 import laser_polio as lp
 
-"""
-This script contains a demo simulation of polio transmission in Nigeria.
-
-The model uses the same data and setup as the EMOD model, except in the following instances:
-- The model assumes everyone >15y is immune
-- The total population counts are being estimated by scaling up u5 population counts based on their proportion of the population
-- I'm using a sinusoidal seasonality function rather than a step function
-- The nodes are not divided below the adm2 level (with no plans to do so)
-- There is no scaling of transmission between N & S Nigeria (other than underweight fraction)
-- We do not update the cbr, ri, sia, or underwt data over time
-- Vaccines are not allowed to transmit
-"""
-
 ###################################
 ######### USER PARAMETERS #########
 
@@ -40,7 +27,6 @@ sim = lp.run_sim(
     init_prev=init_prev,
     r0=r0,
     results_path=results_path,
-    params_file="params.json",
 )
 
 sc.printcyan("Done.")
